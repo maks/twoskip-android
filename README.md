784046a0f0f72b68a4b9729ced1a2c3a75c813d8
Maksim Lin <maks@manichord.com>
2016-02-05 22:27:40 +1100

    initial project skeleton

---
+++ README.md
@@ -0,0 +1,15 @@
+# Twoskip key-value database for Android
+
+## About
+
+This is mostly a port of the twoskip key-value databse implementation from the [Cyrus IMAP server](https://cgit.cyrus.foundation/cyrus-imapd/tree/lib/cyrusdb_twoskip.c)
+
+It provides
+
+[See also this slide deck](http://opera.brong.fastmail.fm.user.fm/talks/twoskip/twoskip-yapc12.pdf)
+
+[Cyrus key-value db API](http://fossies.org/linux/cyrus-imapd/doc/internal/cyrusdb-api.html)
+
+## Usage
+
+### API
---
+++ twoskipandroid/src/main/java/com/manichord/twoskip/FileDB.java
@@ -0,0 +1,48 @@
+package com.manichord.twoskip;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+/**
+ *
+ */
+public class FileDB {
+
+
+    public FileDB(FileInputStream store) {
+
+    }
+
+
+    public void open(File dbFile) {
+
+    }
+
+    public void close() {
+
+    }
+
+    public void get() {
+
+    }
+
+    public void set() {
+
+    }
+
+    public void delete() {
+
+    }
+
+    public void forEach() {
+
+    }
+
+    public void commit() {
+
+    }
+
+    public void abort() {
+
+    }
+}
